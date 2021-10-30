1a7e604be80dce3e999a89e1e996910bbb0f4d6c
Sandro Wrzalek <sandro.wrzalek@lazydatascientist.com>
2021-10-30 19:10:55 +0200

    Improve admin interface

--- resume/admin.py
+++ resume/admin.py
@@ -3,6 +3,35 @@ from .models import Profile, Testimonial, Certificate, SoftSkill, TechnicalSkill
 
 
 # Register your models here.
+@admin.register(TechnicalSkill)
+class TechnicalSkillAdmin(admin.ModelAdmin):
+    list_display = ('id',
+                    'name',
+                    'score',
+                    'is_active',
+                    )
+    list_filter = ('is_active',
+                   )
+    list_editable = ('score',
+                     'is_active',
+                     )
+    search_fields = ('name',
+                     )
+
+
+@admin.register(SoftSkill)
+class SoftSkillAdmin(admin.ModelAdmin):
+    list_display = ('id',
+                    'name',
+                    'is_active')
+    list_filter = ('is_active',
+                   )
+    list_editable = ('is_active',
+                     )
+    search_fields = ('name',
+                     )
+
+
 @admin.register(Profile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('id', 'main_profile')
@@ -10,19 +39,32 @@ class UserProfileAdmin(admin.ModelAdmin):
 
 @admin.register(Testimonial)
 class TestimonialAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'is_active')
+    list_display = ('id', 'first_name',
+                    'last_name',
+                    'is_active',
+                    )
+    list_filter = ('is_active',
+                   )
+    list_editable = ('is_active',
+                     )
+    search_fields = ('first_name',
+                     'last_name',
+                     'role',
+                     )
 
 
 @admin.register(Certificate)
 class CertificateAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'institution')
-
-
-@admin.register(SoftSkill)
-class SoftSkillAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'is_active')
-
-
-@admin.register(TechnicalSkill)
-class TechnicalSkillAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'score', 'is_active')
+    list_display = ('id',
+                    'title',
+                    'institution',
+                    'date_received',
+                    'is_active')
+    list_filter = ('is_active',
+                   )
+    list_editable = ('is_active',
+                     )
+    search_fields = ('title',
+                     'institution',
+                     'is_active',
+                     )
