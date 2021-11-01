b8736026e624643339713e207b514a8303c7ef6f
Sandro Wrzalek <sandro.wrzalek@lazydatascientist.com>
2021-11-01 16:17:06 +0100

    Rename application from personal_site to mango

--- mango/asgi.py
+++ mango/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for personal_site project.
+ASGI config for mango project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mango.settings')
 
 application = get_asgi_application()
--- mango/urls.py
+++ mango/urls.py
@@ -1,4 +1,4 @@
-"""personal_site URL Configuration
+"""mango URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -11,7 +11,7 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('personal_site/', include('personal_site.urls'))
+    2. Add a URL to urlpatterns:  path('mango/', include('mango.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
