919f7bd111a503d3654f99458b53b3537dd3d070
Sandro Wrzalek <6593103-sawrz@users.noreply.gitlab.com>
2021-11-05 10:34:53 +0100

    Move static and media files directories

--- mango/settings.py
+++ mango/settings.py
@@ -130,10 +130,10 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'files/staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'files/mediafiles'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
