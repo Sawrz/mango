177448d79b18254041e7fdfb17749204d30a0c88
Sandro Wrzalek <6593103-sawrz@users.noreply.gitlab.com>
2021-11-14 19:38:54 +0100

    Put app_name first to match projects style

--- main/urls.py
+++ main/urls.py
@@ -2,13 +2,12 @@ from django.urls import path
 from django.conf import settings
 from . import views
 
+app_name = 'main'
 
 if settings.LAUNCHED:
     indexView = views.MaintenanceView
 else:
     indexView = views.SoonView
-
-app_name = 'main'
 
 urlpatterns = [
     path('', indexView.as_view(), name='home'),
