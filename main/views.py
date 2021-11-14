78b62eb71bea0dc76efea78bb3ae99e1359f0801
Sandro Wrzalek <6593103-sawrz@users.noreply.gitlab.com>
2021-11-14 19:42:04 +0100

    Add Login and logout view

--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
 from django.views import generic
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+
 
 # Create your views here.
 class SoonView(generic.TemplateView):
@@ -7,3 +10,15 @@ class SoonView(generic.TemplateView):
 
 class MaintenanceView(generic.TemplateView):
     template_name = 'main/maintenance.html'
+
+
+class CreatorLoginView(LoginView):
+    template_name = 'registration/login.html'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('create:dashboard')
+
+
+class CreatorLogoutView(LogoutView):
+    template_name = 'registration/logout.html'
